051a7d241340f9b7c15d13e6b7e9fbf6cc343a10
Alec Rosenbaum <git@alecrosenbaum.com>
2024-12-27 07:12:25 -0500

    move canvas entirely into rust, add sidebar w/ basic inputs

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,136 @@
 use std::f64;
 use wasm_bindgen::prelude::*;
+use web_sys::HtmlInputElement;
 use std::cmp::max;
-use js_sys::Array;
 use web_sys::console;
+use std::rc::Rc;
+use std::cell::RefCell;
 
-const SCALING_FACTOR: f64 = 4.0;
 
 #[wasm_bindgen(start)]
-fn start() {
+fn start() -> Result<(), JsValue> {
     let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .unwrap();
+    let canvas = document
+        .create_element("canvas")?
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+    document.body().unwrap().append_child(&canvas)?;
+
+    // create page state
+    let page_state = PageState {
+        left_gear_spec: GearSpecs {
+            teeth: 50.0,
+            module: 15.0,
+            tooth_angle: 20.0,
+            clearance_mult: 0.167,
+            backlash_mult: 0.05,
+        },
+        right_gear_spec: GearSpecs {
+            teeth: 10.0,
+            module: 15.0,
+            tooth_angle: 20.0,
+            clearance_mult: 0.167,
+            backlash_mult: 0.05,
+        },
+    };
+    let page_state_rc = Rc::new(RefCell::new(page_state));
+
+    // setup canvas drawing context + do initial redraw
     let context = canvas
         .get_context("2d")
         .unwrap()
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
-
-    full_redraw(&canvas, &context);
+    full_redraw(&canvas, &context, &page_state_rc.borrow());
     
     // Add event listener for window resize + redraw
-    let closure = Closure::wrap(Box::new(move || { full_redraw(&canvas, &context); }) as Box<dyn Fn()>);
+    let page_state_rc_clone = page_state_rc.clone();
+    let closure = Closure::wrap(Box::new(move || { full_redraw(&canvas, &context, &page_state_rc_clone.borrow()); }) as Box<dyn Fn()>);
     web_sys::window().unwrap().add_event_listener_with_callback("resize", closure.as_ref().unchecked_ref()).unwrap();
+
+    // create left sidebar
+    let sidebar = create_sidebar(page_state_rc, &closure)?;
+    document.body().unwrap().append_child(&sidebar)?;
     closure.forget();
 
+    Ok(())
 }
 
-fn full_redraw(canvas: &web_sys::HtmlCanvasElement, context: &web_sys::CanvasRenderingContext2d) {
+fn create_sidebar(state: Rc<RefCell<PageState>>, redraw_closure: &Closure<dyn Fn()>) -> Result<web_sys::Element, JsValue> {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let sidebar = document.create_element("div")?;
+    sidebar.set_attribute("style", "position: fixed; left: 0; top: 0; width: 200px; height: 100%; background-color: #f0f0f0;").unwrap();
+
+    // add title
+    let title = document.create_element("h2")?;
+    title.set_attribute("style", "text-align: center; width: 100%;").unwrap();
+    title.set_text_content(Some("Gear Designer"));
+    sidebar.append_child(&title)?;
+
+    // add left gear subtitle
+    let left_gear_subtitle = document.create_element("h3")?;
+    left_gear_subtitle.set_attribute("style", "text-align: center; width: 100%;").unwrap();
+    left_gear_subtitle.set_text_content(Some("Left Gear"));
+    sidebar.append_child(&left_gear_subtitle)?;
+
+    // add text input for left gear teeth
+    let left_gear_input = document.create_element("input")?;
+    left_gear_input.set_attribute("id", "left_gear_teeth").unwrap();
+    left_gear_input.set_attribute("type", "text").unwrap();
+    left_gear_input.set_attribute("placeholder", "Enter left gear teeth").unwrap();
+    left_gear_input.set_attribute("value", &state.borrow().left_gear_spec.teeth.to_string()).unwrap();
+    left_gear_input.set_attribute("style", "width: 80%; margin-left: 10%; margin-right: 10%;").unwrap();
+    sidebar.append_child(&left_gear_input)?;
+
+    // add right gear subtitle
+    let right_gear_subtitle = document.create_element("h3")?;
+    right_gear_subtitle.set_attribute("style", "text-align: center; width: 100%;").unwrap();
+    right_gear_subtitle.set_text_content(Some("Right Gear"));
+    sidebar.append_child(&right_gear_subtitle)?;
+
+    // add right gear input
+    let right_gear_input = document.create_element("input")?;
+    right_gear_input.set_attribute("id", "right_gear_teeth").unwrap();
+    right_gear_input.set_attribute("type", "text").unwrap();
+    right_gear_input.set_attribute("placeholder", "Enter right gear teeth").unwrap();
+    right_gear_input.set_attribute("value", &state.borrow().right_gear_spec.teeth.to_string()).unwrap();
+    right_gear_input.set_attribute("style", "width: 80%; margin-left: 10%; margin-right: 10%;").unwrap();
+    sidebar.append_child(&right_gear_input)?;
+
+    // Add all event listeners to update state when input changes
+    let closure = Closure::wrap(Box::new(move || {
+        // get left gear input
+        let value = left_gear_input.dyn_ref::<HtmlInputElement>().unwrap().value();
+        if let Ok(teeth) = value.parse::<u32>() {
+            // Borrow the state mutably to update it
+            state.borrow_mut().left_gear_spec.teeth = teeth as f64; // Update the state
+        }
+        // get right gear input
+        let value = right_gear_input.dyn_ref::<HtmlInputElement>().unwrap().value();
+        if let Ok(teeth) = value.parse::<u32>() {
+            // Borrow the state mutably to update it
+            state.borrow_mut().right_gear_spec.teeth = teeth as f64; // Update the state
+        }
+    }) as Box<dyn Fn()>);
+
+    sidebar.add_event_listener_with_callback("input", closure.as_ref().unchecked_ref())?;
+    closure.forget();
+
+    // redraw after input is changed / parameters are updated
+    sidebar.add_event_listener_with_callback("input", redraw_closure.as_ref().unchecked_ref())?;
+
+    Ok(sidebar)
+}
+
+
+fn full_redraw(canvas: &web_sys::HtmlCanvasElement, context: &web_sys::CanvasRenderingContext2d, page_state: &PageState) {
     let width = calculate_window_width();
     let height = calculate_window_height();
-    canvas.set_width(width);
+    canvas.set_attribute("style", "padding-left: 200px;").unwrap();
+    canvas.set_width(width - 200);
     canvas.set_height(height);
     context.translate(width as f64 / 2.0, height as f64 / 2.0).unwrap(); // now 0,0 is the center of the canvas.
-    redraw(context, width, height);
+    redraw(context, width, height, page_state);
 }
 
 // enum left / right
@@ -62,6 +156,12 @@ impl std::ops::Add for Point {
             y: self.y + other.y,
         }
     }
+}
+
+// struct for page state
+struct PageState {
+    left_gear_spec: GearSpecs,
+    right_gear_spec: GearSpecs,
 }
 
 // struct for gear specs
@@ -92,35 +192,21 @@ impl DebugConfig {
     }
 }
 
-fn redraw(context: &web_sys::CanvasRenderingContext2d, width: u32, height: u32) {
+fn redraw(context: &web_sys::CanvasRenderingContext2d, width: u32, height: u32, page_state: &PageState) {
     context.clear_rect(0.0, 0.0, width as f64, height as f64);
     draw_background(context, width, height);
 
     let debug_config = DebugConfig::default();
 
     // Draw left gear (circle for now)
-    let gear_1_spec = GearSpecs {
-        teeth: 50.0,
-        module: 15.0,
-        tooth_angle: 20.0,
-        clearance_mult: 0.167,
-        backlash_mult: 0.05,
-    };
-    draw_gear(context, Gear::Left, gear_1_spec, &debug_config);
+    draw_gear(context, Gear::Left, &page_state.left_gear_spec, &debug_config);
 
     // Draw right gear (circle for now)
-    let gear_2_spec = GearSpecs {
-        teeth: 10.0,
-        module: 15.0,
-        tooth_angle: 20.0,
-        clearance_mult: 0.167,
-        backlash_mult: 0.05,
-    };
-    draw_gear(context, Gear::Right, gear_2_spec, &debug_config);
+    draw_gear(context, Gear::Right, &page_state.right_gear_spec, &debug_config);
 }
 
 
-fn draw_gear(context: &web_sys::CanvasRenderingContext2d, left_or_right: Gear, gear_spec: GearSpecs, debug_config: &DebugConfig) {
+fn draw_gear(context: &web_sys::CanvasRenderingContext2d, left_or_right: Gear, gear_spec: &GearSpecs, debug_config: &DebugConfig) {
     // Gear specifications
     let teeth = gear_spec.teeth;
     let module = gear_spec.module;
@@ -193,7 +279,7 @@ fn draw_gear(context: &web_sys::CanvasRenderingContext2d, left_or_right: Gear, g
     }
 
     context.set_stroke_style_str("black");
-    context.set_line_dash(&JsValue::from(Vec::<f64>::new()));
+    context.set_line_dash(&JsValue::from(Vec::<f64>::new())).unwrap();
     context.begin_path();
     
     // draw all teeth
